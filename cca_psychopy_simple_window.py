b03b1409604c826e0290d0a8bb0e349c94ba4110
Matthew Heim <matt@heimfamily.org>
2020-11-23 18:57:21 -0500

    make new branch

---
+++ cca_psychopy_simple_window.py
@@ -0,0 +1,178 @@
+# live predicting window, and import csv as user input
+# arrow should point to the correct input
+# TODO: perhaps a square around the chosen input 
+# used some parts from data_collection_window, command_handler, gaming_window
+
+import os
+from project_constants import FREQUENCY, PATTERN_LENGTH, PADDING, BASE_HEIGHT, BASE_WIDTH, ARROW_SIZE, ARROW_SCALE, \
+    CHECKERBOARD_SIZE, RECORDINGS_PER_ICON, SECONDS_PER_RECORDING, COMMAND_SEND_FREQUENCY
+import pandas as pd
+import numpy as np
+# from pyboy import PyBoy
+# from pyboy.utils import WindowEvent
+# import sdl2
+# from command_handler import command_handler
+from cca_train import cca_handler
+import ctypes
+import time
+import re
+from psychopy import core, visual
+
+
+class live_predicting_window():
+
+    def __init__(self, recording_name='recordings/first_target_0.csv'):
+
+        # set frame rate as (1 / desired_frame_rate)
+        # self.set_update_rate(1/FREQUENCY)
+
+        # data collection vars
+        self.generator = None
+        # recording data is imported from csv
+        self.recording_data = []
+
+        # length of recording data
+        self.record_length = None
+        self.tick = 0
+
+        # row number index
+        self.row_index = 0
+
+        self.channels = ['O1', 'O2']
+
+        self.display_target = None # psychopy text object
+        self.target = None # number
+
+
+        # ML vars
+        self.command_handler = None
+
+        # psychopy vars
+        self.win = None
+
+        self.running = False
+
+        # set recording name
+        self.recording_name = recording_name
+
+        # import
+        self.import_recording()
+
+    def print_stage(self, stage):
+        print('-'*20)
+        print('{} BEGINNING'.format(stage))
+        print('-'*20)
+
+    def setup(self):
+        self.print_stage("VISUALS SETUP")
+        self.setup_visuals()
+        self.print_stage("COMMAND HANDLER SETUP")
+        self.setup_command_handler()
+        print('FREQUENCY = {} \t COMMAND_SEND_FREQUENCY = {}'.format(FREQUENCY, COMMAND_SEND_FREQUENCY))
+        self.running = True # ready to run 
+        
+    def setup_visuals(self):
+        self.win = visual.Window([400, 300])
+        self.display_target = visual.TextStim(self.win, text="Predicted target:")
+        self.display_target.draw()
+        self.win.flip()
+
+    def setup_command_handler(self):
+        # choose the desired command handler
+        self.command_handler = cca_handler()
+
+    # def on_update(self, delta_time):
+    #     # check if done
+    #     if self.row_index >= self.record_length:
+    #         print('-'*20)
+    #         print('Done reading file. Exiting')
+    #         print('-'*20)
+    #         self.exit_window()
+    #     # self.pyboy.tick()
+    #     self.tick += 1
+    #     self.tick %= FREQUENCY
+    #     if self.tick % COMMAND_SEND_FREQUENCY == 0:
+    #         start = time.time()
+    #         self.target = self.command_handler.predict(self.get_eeg_data())
+    #         self.on_draw()
+    #         # print("Guess done in: {}s".format(time.time() - start))
+    #         self.row_index += 128
+
+    # def exhaust(self):
+    #   next(self.generator)
+    
+    def on_draw(self):
+        self.display_target.text = str(self.target)
+        self.display_target.draw()
+        self.win.flip()
+
+    def get_eeg_data(self):
+        # return np.asarray(list(next(self.generator).queue))
+        # get 128 rows of recording_data
+        return np.asarray(self.recording_data[self.channels][self.row_index:self.row_index + 128])
+
+
+    def import_recording(self):
+        # make sure name is ok
+        if 'recordings/' not in self.recording_name:
+            self.recording_name = 'recordings/' + self.recording_name
+        if '.csv' not in self.recording_name:
+            self.recording_name += '.csv'
+        self.recording_data = pd.read_csv(self.recording_name)
+        self.record_length = len(self.recording_data.index)
+
+    def on_key_press(self, key, key_modifiers):
+        actions = []
+        print("KEY PRESSED: {}".format(key))
+
+        # switch statement to create WindowEvents (button commands for PyBoy)
+#       if (key == arcade.key.UP):
+#           actions = [WindowEvent.PRESS_ARROW_UP, WindowEvent.RELEASE_ARROW_UP]
+#       elif (key == arcade.key.DOWN):
+#           actions = [WindowEvent.PRESS_ARROW_DOWN, WindowEvent.RELEASE_ARROW_DOWN]
+#       elif (key == arcade.key.LEFT):
+#           actions = [WindowEvent.PRESS_ARROW_LEFT, WindowEvent.RELEASE_ARROW_LEFT]
+#       elif (key == arcade.key.RIGHT):
+#           actions = [WindowEvent.PRESS_ARROW_RIGHT, WindowEvent.RELEASE_ARROW_RIGHT]
+#       elif (key == arcade.key.Z):
+#           actions = [WindowEvent.PRESS_BUTTON_B, WindowEvent.RELEASE_BUTTON_B]
+#       elif (key == arcade.key.X):
+#           actions = [WindowEvent.PRESS_BUTTON_A, WindowEvent.RELEASE_BUTTON_A]
+#       elif (key == arcade.key.ENTER):
+#           actions = [WindowEvent.PRESS_BUTTON_SELECT, WindowEvent.RELEASE_BUTTON_SELECT]
+#       elif (key == arcade.key.RSHIFT):
+#           actions = [WindowEvent.PRESS_BUTTON_START, WindowEvent.RELEASE_BUTTON_START]
+
+        # if actions list isn't empty, send commands to pyboy and tick the pyboy window to process each command
+#       if actions:
+#           for action in actions:
+#               self.pyboy.send_input(WindowEvent(action))
+#               self.pyboy.tick()
+
+    def exit_window(self):
+        print('-'*20)
+        print('Done reading file. Exiting')
+        print('-'*20)
+        self.running = False
+        self.win.close()
+        core.quit()
+        exit()
+
+    def run(self):
+        while self.running:
+            
+            if self.row_index >= self.record_length:
+                # done and exit
+                self.exit_window()
+
+            core.wait(1.0)
+            self.target = self.command_handler.predict(self.get_eeg_data())
+            self.on_draw()
+            # print("Guess done in: {}s".format(time.time() - start))
+            self.row_index += 128
+
+
+if __name__ == "__main__":
+    window = live_predicting_window('recordings/first_target_1')
+    window.setup()
+    window.run()
---
+++ command_handler.py
@@ -0,0 +1,95 @@
+# from pynput.keyboard import Key, Controller
+# import pywt
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import minmax_scale
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+from joblib import load
+
+class command_handler():
+    ml_model = None
+    pca = None
+    # keyboard = Controller()
+
+    def __init__(self):
+        self.load_model_from_json()
+        self.ml_model.predict(np.ones((1,12,128)))
+        self.pca = load('models\pca')
+
+    def load_model_from_json(self):
+        with open("models\model.json", 'r') as json_file:
+            json_model = json_file.read()
+        self.ml_model = tf.keras.models.model_from_json(json_model)
+        self.ml_model.load_weights("models\model.h5")
+
+    def filter_data(self, eeg_data):
+        eeg_data = eeg_data[:,:-1]
+        print(eeg_data[:5,:])
+        approx, decomp = self.wavelet_dwt(eeg_data)
+        pca_approx = self.pca_and_inverse(approx)
+        pca_decomp = self.pca_and_inverse(decomp)
+        total_data = np.concatenate((pca_approx, pca_decomp), axis=1)
+        data = self.scale_and_normalize(total_data)
+        return self.reformat_data(data)
+
+#    def wavelet_dwt(self, eeg_data):
+#        return pywt.dwt(eeg_data, 'db5')
+
+    def pca_and_inverse(self, data):
+        self.pca.fit(data)
+        components = self.pca.transform(data)
+        return self.pca.inverse_transform(components)
+
+    def scale_and_normalize(self, data):
+        for col in range(data.shape[1]):
+            data[:, col] = minmax_scale(data[:, col])
+        return data
+
+    def reformat_data(self, data):
+        data = np.transpose(data)
+        data = data.reshape(-1, data.shape[0], data.shape[1])
+        return data
+
+    def predict_command(self, data):
+        pred = self.ml_model.predict(data)
+        command = np.argmax(pred)
+        return command
+
+#    def press_release(self, key):
+#        self.keyboard.press(key)
+#        self.keyboard.release(key)
+
+#    def command_to_keyboard_action(self, command):
+#        # command 0 is no-action
+#        if command == 0:
+#            return
+#        elif command == 1:
+#            self.press_release('x')
+#        elif command == 2:
+#            self.press_release(Key.up)
+#        elif command == 3:
+#            self.press_release('z')
+#        elif command == 4:
+#            self.press_release(Key.left)
+#        elif command == 5:
+#            self.press_release(Key.right)
+#        elif command == 6:
+#            self.press_release(Key.enter)
+#        elif command == 7:
+#            self.press_release(Key.down)
+#        elif command == 8:
+#            self.press_release(Key.shift_r)
+
+    def predict(self, eeg_data):
+        # filter data
+        print("Filter Data")
+        data = self.filter_data(eeg_data)
+        # make command prediction
+        print("Make Prediction")
+        command = self.predict_command(data)
+        # map command to a keyboard action
+        print("Predicted Command: {}".format(command))
+        self.command_to_keyboard_action(command)
