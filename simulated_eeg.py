6cfc1fcf688a4d9cbb2674e675f3a16dd9c4628c
Matthew Heim <matt@heimfamily.org>
2020-12-03 15:34:22 -0500

    looking at power spectrum

--- simulated_eeg.py
+++ simulated_eeg.py
@@ -34,15 +34,14 @@ def plot_time_all(timeList, data, start=0, end=1000):
 		fig.savefig('simulated_recordings/simulated_1s_plots.png')
 		print('time plots saved.')
 
-def plot_power_all(timeList, data):
+def plot_power_all(data):
 	# plot power spectrum - list of df
 	# use median average power spectrum
-	fig, axs = plt.subplots(2, 2, figsize=(15, 10), constrained_layout=True)
+	fig, axs = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
 	for i in range(len(data)):
 		for color, channel in enumerate(channels):
 			sig = np.asarray(data[i][channel])
 			freqs, pows = signal.welch(sig, 128.0, nperseg=128, average='median')
-			# plt.semilogy(freqs, pows, label=channel, color=my_colors[color])
 			axs.flatten()[i].plot(freqs, pows, label=channel, color=my_colors[color])
 		axs.flatten()[i].set_yscale('log')
 		axs.flatten()[i].set_title('Target {} ({} Hz) Power Spectrum'.format(i, target_freqs[i]))
@@ -60,24 +59,22 @@ if save and not os.path.exists('simulated_recordings'):
 			os.makedirs('simulated_recordings')
 
 
+sample_rate = 128.0
+num_seconds = 100
+samples = int(sample_rate * num_seconds)
+timeList = np.arange(0, num_seconds, 1/sample_rate)
+
 df_cols = ['Counter', 'P7', 'O1', 'O2', 'P8']
 num_targets = 4
 target_df_list = []
 for i in range(num_targets):
 	target_df = pd.DataFrame(columns=df_cols)
+	target_df['Counter'] = [i % sample_rate for i in range(samples)]
 	target_df_list.append(target_df)
-
-sample_rate = 128.0
-num_seconds = 100
-samples = int(sample_rate * num_seconds)
 
 mean = 0
 stdev = 2
 variance = stdev ** 2
-# time goes from 0 to number of seconds with spacing of 1/sample_rate
-# sampling frequency is 128 Hz - 100 sec of data
-timeList = np.arange(0, num_seconds, 1/sample_rate)
-
 
 second_harmonic = target_freqs * 2
 
@@ -106,13 +103,13 @@ for i in range(num_targets):
 		target_signal_channel = np.sin(2 * np.pi * target_freqs[i] * timeList) * multiply_noise + drift + white_noise + all_channel_random
 		sec_mul_noise = np.random.uniform(0.5, 1.5, samples)
 		sec_add_noise = np.random.normal(0, stdev/8, samples)
-		second_harmonic_signal_channel = 0.2 * np.sin(2 * np.pi * second_harmonic[i] * timeList) * sec_mul_noise + sec_add_noise
+		second_harmonic_signal_channel = 1.0 * np.sin(2 * np.pi * second_harmonic[i] * timeList) * sec_mul_noise + sec_add_noise
 		target_df_list[i][channel] = target_signal_channel + second_harmonic_signal_channel
 
 # have to close plot before saving files.
 plot_time_all(timeList, target_df_list, start=0, end=128)
 
-plot_power_all(timeList, target_df_list)
+plot_power_all(target_df_list)
 
 if save:
 	for i in range(num_targets):
